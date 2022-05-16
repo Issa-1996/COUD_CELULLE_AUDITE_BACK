c70c48de72f1ddf66d9d1cd88b6c5b3985f5322f
unknown <cheikhibra1988@gmail.com>
2022-05-16 08:45:53 +0200

    pull

--- auditBack/src/DataFixtures/AppFixtures.php
+++ auditBack/src/DataFixtures/AppFixtures.php
@@ -10,27 +10,27 @@ use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 
 class AppFixtures extends Fixture
 {
-    // public function __construct(UserPasswordEncoderInterface $passwordEncoder){
-    //     $this->passwordEncoder = $passwordEncoder;
-    // }
+    public function __construct(UserPasswordEncoderInterface $passwordEncoder){
+        $this->passwordEncoder = $passwordEncoder;
+    }
     public function load(ObjectManager $manager): void
     {
-            // $role= ["ROLE_COORDONATEUR"];
-            // $user=new User();
-            // $profil=new Profil();
-            // $profil->setLibelle("COORDINATEUR");
-            // $user->setUsername("admin");
-            // $user->setRoles($role);
-            // $user->setProfil($profil);
-            // $user->setPrenom("Issa");
-            // $user->setNom("SARR");
-            // $user->setMatricule("23456");
-            // $user->setDateDeNaissance("10/10/2020");
-            // $user->setEmail("issa.cheikhbi.gnilane@gmail.com");
-            // $password = $this->passwordEncoder->encodePassword($user,'password');
-            // $user->setPassword($password);
+            $role= ["ROLE_COORDONATEUR"];
+            $user=new User();
+            $profil=new Profil();
+            $profil->setLibelle("COORDINATEUR");
+            $user->setUsername("admin");
+            $user->setRoles($role);
+            $user->setProfil($profil);
+            $user->setPrenom("Issa");
+            $user->setNom("SARR");
+            $user->setMatricule("23456");
+            $user->setDateDeNaissance("10/10/2020");
+            $user->setEmail("issa.cheikhbi.gnilane@gmail.com");
+            $password = $this->passwordEncoder->encodePassword($user,'password');
+            $user->setPassword($password);
     
-            // $manager->persist($user);
-            // $manager->flush();
+            $manager->persist($user);
+            $manager->flush();
     }
 }
